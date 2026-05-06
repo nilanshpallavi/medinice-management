f483c6b262bda9c320c3629b9943b354cbe20b0e
nilansh.pallavi <nilansh.pallavi53@gmail.com>
2026-05-07 03:24:36 +0530

    Adding new files

---
+++ MedicineManagement/Model/medicine.cs
@@ -0,0 +1,10 @@
+public class Medicine
+{
+    public int id { get; set; }
+    public string fullName { get; set; }
+    public string notes { get; set; }
+    public string expiryDate { get; set; }
+    public int quantity { get; set; }
+    public decimal price { get; set; }
+    public string brand { get; set; }
+}
--- MedicineManagement/obj/Release/net9.0/MedicineManagement.AssemblyInfo.cs
+++ MedicineManagement/obj/Release/net9.0/MedicineManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicineManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83631e631abb42e08f0f6d6041382dfe8808887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559218290245e33c0c9105bc2b82666012da2f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicineManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicineManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
